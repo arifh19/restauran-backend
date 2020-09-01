660158f2fa7c0e11e84ebb400a34b9e3f6bac89d
Arif Hidayat <arif.h@mail.ugm.ac.id>
2020-09-01 11:16:27 +0700

    first commit

--- src/helper/validator.js
+++ src/helper/validator.js
@@ -3,7 +3,7 @@ const validation = {}
 
 const validator = (body, rules, customMessages, callback) => {
     const validation = new Validator(body, rules, customMessages)
-    validation.passes(() => callback(null, true));
+    validation.passes(() => callback(null, true))
     validation.fails(() => callback(validation.errors, false))
 }
 
