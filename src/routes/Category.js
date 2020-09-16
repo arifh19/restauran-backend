814035e699d2d64146aa1ab3fdfef1ac8693e49c
Arif Hidayat <arif.h@mail.ugm.ac.id>
2020-09-17 06:53:03 +0700

    route

--- src/routes/Category.js
+++ src/routes/Category.js
@@ -5,9 +5,15 @@ const cache = require('../middleware/cache')
 const authorize = require('../middleware/authorize')
 const isLogin = require('../middleware/validate')
 
-route.get('/', isLogin, authorize.grantAccess(['admin', 'operator']), cache, controller.all)
-route.post('/', isLogin, authorize.grantAccess(['admin']), controller.add)
-route.put('/', isLogin, authorize.grantAccess(['admin']), controller.edit)
-route.delete('/', isLogin, authorize.grantAccess(['admin']), controller.delete)
+// route.get('/', isLogin, authorize.grantAccess(['admin', 'operator']), cache, controller.all)
+// route.post('/', isLogin, authorize.grantAccess(['admin']), controller.add)
+// route.put('/', isLogin, authorize.grantAccess(['admin']), controller.edit)
+// route.delete('/', isLogin, authorize.grantAccess(['admin']), controller.delete)
+
+
+route.get('/', cache, controller.all)
+route.post('/', controller.add)
+route.put('/', controller.edit)
+route.delete('/', controller.delete)
 
 module.exports = route
